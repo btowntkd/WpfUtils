d6973e03d7b2e5c608707158ecf58f5b6dc990b5
Brian Pratt <btowntkd@gmail.com>
2013-05-28 09:30:53 -0400

    Added Int32Parts and UInt32Parts utility classes.

---
+++ Int32Parts.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WpfUtils
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Int32Parts
+    {
+        #region Public Fields
+
+        [FieldOffset(0)]
+        public Int32 Value;
+
+        [FieldOffset(0)]
+        public UInt16 LowWord;
+
+        [FieldOffset(2)]
+        public Int16 HighWord;
+
+        #endregion
+
+        #region Constructors
+
+        public Int32Parts(Int32 value)
+        {
+            //Low and High words must be assigned in constructor;
+            //simply zero them out, and the "Value" field will overwrite them
+            this.LowWord = 0;
+            this.HighWord = 0;
+            this.Value = value;
+        }
+
+        public Int32Parts(UInt16 low, Int16 high)
+        {
+            this.Value = 0;
+            this.LowWord = low;
+            this.HighWord = high;
+        }
+
+        #endregion
+
+        #region Public Operators
+
+        public static implicit operator Int32Parts(Int32 value)
+        {
+            return new Int32Parts(value);
+        }
+
+        public static implicit operator Int32(Int32Parts parts)
+        {
+            return parts.Value;
+        }
+
+        #endregion
+    }
+}
