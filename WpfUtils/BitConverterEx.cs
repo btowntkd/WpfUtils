defb1602922da4b59feb11250ea37727ad822127
Brian Pratt <bcpratt@live.com>
2013-10-03 13:08:25 -0400

    Added BitConverterEx class and tests.

---
+++ WpfUtils/BitConverterEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtils
+{
+    /// <summary>
+    /// Provides additional BitConverter functionality.
+    /// </summary>
+    public static class BitConverterEx
+    {
+        public static byte[] GetBytes(UInt16[] words)
+        {
+            return words.SelectMany((x) => BitConverter.GetBytes(x)).ToArray();
+        }
+    }
+}
---
+++ WpfUtilsTests/BitConverterExTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUtils;
+
+namespace WpfUtilsTest
+{
+    [TestClass]
+    public class BitConverterExTests
+    {
+        [TestMethod]
+        public void GetBytes_FromWords()
+        {
+            ushort[] input = { 0x0102, 0x0304, 0x0506, 0x0708 };
+            byte[] expectedOutput = { 0x02, 0x01, 0x04, 0x03, 0x06, 0x05, 0x08, 0x07 };
+
+            byte[] actualOutput = BitConverterEx.GetBytes(input);
+
+            Assert.AreEqual(BitConverter.ToString(expectedOutput), BitConverter.ToString(actualOutput), "Byte arrays were not equal");
+        }
+    }
+}
