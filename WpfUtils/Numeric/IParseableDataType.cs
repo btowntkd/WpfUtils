ca73f127b66e2a924dc8aafe0e644a41c2407b1f
Brian Pratt <bcpratt@live.com>
2013-11-26 14:59:26 -0500

    Started work on interface for parse-able numerics.

---
+++ WpfUtils/Numeric/IParseableDataType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtils.Numeric
+{
+    /// <summary>
+    /// Interface for data types which support the ability to parse their values from a string.
+    /// </summary>
+    /// <typeparam name="TDataType">The underlying data type which is able to be parsed from a string.</typeparam>
+    public interface IParseableDataType<TDataType>
+    {
+        /// <summary>
+        /// Parse and set the current value based on the provided string.
+        /// </summary>
+        /// <param name="value">The string value to parse.</param>
+        void Parse(string value);
+
+        /// <summary>
+        /// Get or Set the current value of the underlying data.
+        /// </summary>
+        TDataType Value { get; set; }
+    }
+}
