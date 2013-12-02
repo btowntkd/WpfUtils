bda3acc1ccffd67504b6a91531e8d1fdf7d6d137
Brian Pratt <bcpratt@live.com>
2013-12-01 21:41:35 -0500

    Added IProvideRawData interface. Implemented ParseableInt class.

--- WpfUtils/Numeric/IParseableDataType.cs
+++ WpfUtils/Numeric/IParseableDataType.cs
@@ -9,18 +9,12 @@ namespace WpfUtils.Numeric
     /// <summary>
     /// Interface for data types which support the ability to parse their values from a string.
     /// </summary>
-    /// <typeparam name="TDataType">The underlying data type which is able to be parsed from a string.</typeparam>
-    public interface IParseableDataType<TDataType>
+    public interface IParseableDataType
     {
         /// <summary>
         /// Parse and set the current value based on the provided string.
         /// </summary>
         /// <param name="value">The string value to parse.</param>
         void Parse(string value);
-
-        /// <summary>
-        /// Get or Set the current value of the underlying data.
-        /// </summary>
-        TDataType Value { get; set; }
     }
 }
---
+++ WpfUtils/Numeric/IProvideRawData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WpfUtils.Numeric
+{
+    /// <summary>
+    /// Interface through which a data type can provide raw bytes representing the underlying value.
+    /// </summary>
+    interface IProvideRawData
+    {
+        /// <summary>
+        /// Get the raw underlying bytes from the object.
+        /// </summary>
+        Byte[] RawData { get; }
+    }
+}
