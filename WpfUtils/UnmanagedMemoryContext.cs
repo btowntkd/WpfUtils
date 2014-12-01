95cf76821fda00ef005e4506f50f8ce47fdf77db
Brian Pratt <bpratt@pyramidsolutions.com>
2014-12-01 12:50:43 -0500

    Added UnmanagedMemoryContext class for allocating unmanaged memory, with automatic cleanup.

---
+++ WpfUtils/UnmanagedMemoryContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtils
+{
+    /// <summary>
+    /// Provides a disposable, managed replacement for allocating memory using <see cref="Marshal.AllocHGlobal"/> and <see cref="Marshal.FreeHGlobal"/>
+    /// </summary>
+    public class UnmanagedMemoryContext : IDisposable
+    {
+        private readonly object _marshalledDataLock = new object();
+        private readonly Dictionary<IntPtr, int> _marshalledData = new Dictionary<IntPtr,int>();
+
+        public UnmanagedMemoryContext()
+        { }
+
+        ~UnmanagedMemoryContext()
+        {
+            FreeAll();
+        }
+
+        void IDisposable.Dispose()
+        {
+            FreeAll();
+            GC.SuppressFinalize(this);
+        }
+
+        public int TotalAllocated
+        {
+            get 
+            { 
+                lock(_marshalledDataLock)
+                {
+                    return _marshalledData.Values.Sum(); 
+                }
+            }
+        }
+
+        public IntPtr Allocate(int sizeInBytes)
+        {
+            lock (_marshalledDataLock)
+            {
+                var ptr = Marshal.AllocHGlobal(sizeInBytes);
+                _marshalledData.Add(ptr, sizeInBytes);
+                return ptr;
+            }
+        }
+
+        public void FreeAll()
+        {
+            lock (_marshalledDataLock)
+            {
+                foreach (var ptr in _marshalledData.Keys)
+                {
+                    Marshal.FreeHGlobal(ptr);
+                }
+                _marshalledData.Clear();
+            }
+        }
+
+        public void Free(IntPtr ptr)
+        {
+            lock (_marshalledDataLock)
+            {
+                if (_marshalledData.ContainsKey(ptr))
+                {
+                    Marshal.FreeHGlobal(ptr);
+                    _marshalledData.Remove(ptr);
+                }
+            }
+        }
+    }
+}
